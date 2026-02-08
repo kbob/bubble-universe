a92a4d55137de40b4b84e08597c4465fe92cb379
Bob Miller <kbob@jogger-egg.com>
2026-02-08 06:01:36 -0500

    Cleanup, add vecNi types

--- python/wgsl_types.py
+++ python/wgsl_types.py
@@ -1,32 +1,23 @@
 """
-    WGSL types.
+    WGSL (WebGPU Shading Language) types.
 
-    Define scalar, vector, and matrix types matching the types of wgsl.
-    These are immutable.
+    Define data types convenient to use with WGSL shaders.
 
-    Define `Uniforms`, a superclass that allows easy definition of
-    uniform buffers.  Example:
+    This module defines most of WGSL's scalar types and also
+    allows convenient declaration of WGSL uniform buffers.
 
-        class MyUniforms(Uniforms):
-            scale: vec2f = (1, 1)
-            brightness: f32 = 1
-            index: u32
+    These types have class properties:
+    bytes -- size in bytes
+    align -- WGSL alignment requirement in bytes
+    dtype -- a numpy-compatible dtype specification
 
-        # Instantiate into a Python object
-        my_uniforms = MyUniforms(index=3)
+    Vector types also have these.
+    len -- length in elements (e.g., vec3i.len is 3)
+    scalar_type -- its elements' type (e.g., vec3f.scalar_type is f32)
 
-        # Create WGPU uniform buffer
-        my_u_buffer = MyUniforms.create_buffer(device,
-            visibility=Vertex | Fragment)
+    These types are immutable.
 
-        # Update values
-        my_uniforms.scale = (1.4, 1.4)
-
-        # send to GPU
-        my_uniforms.write_buffer(device, my_u_buffer)
-
-
-    NOTE: bool, f16 and matNxN are not implemented yet.
+    The f16, bool, and matrix types aren't impemented (yet?)
 """
 
 from collections import namedtuple
@@ -42,14 +33,23 @@ __all__ = [
     'f32',
     'i32',
     'u32',
+
     'vec2f',
     'vec3f',
     'vec4f',
+    'vec2i',
+    'vec3i',
+    'vec4i',
     'vec2u',
     'vec3u',
     'vec4u',
+
     'Uniforms'
     ]
+
+
+## ##  ##   ##    ##     ##      ##       ##      ##     ##    ##   ##  ## ##
+## Utilities
 
 def camel_to_snake(name):
     return re.sub(r'(?!^)(?=[A-Z])', '_', name).lower()
@@ -64,23 +64,35 @@ class _classproperty:
     def __get__(self, obj, owner):
         return self.f(owner)
 
+
+## ##  ##   ##    ##     ##      ##       ##      ##     ##    ##   ##  ## ##
+## Base
+
 class _WgslType: pass
 
 
+## ##  ##   ##    ##     ##      ##       ##      ##     ##    ##   ##  ## ##
+## The Scalara
+
 class f32(float, _WgslType):
+    """A WGSL 32 bit floating point number"""
     bytes = 4
     align = 4
     dtype = 'f4'
 
 class i32(int, _WgslType):
+    """A WGSL 32 bit signed integer"""
     bytes = 4
     align = 4
     dtype = 'i4'
 
 class u32(int, _WgslType):
+    """A WGSL 32 bit unsigned integer"""
     bytes = 4
     align = 4
     dtype = 'u4'
+
+## Scalar unit tests
 
 x = f32(4)
 assert x == 4.0
@@ -95,15 +107,15 @@ assert isinstance(x, u32)
 
 del x
 
+## ##  ##   ##    ##     ##      ##       ##      ##     ##    ##   ##  ## ##
+## The Vectors
 
 class _Vector(tuple, _WgslType):
 
     def __new__(cls, *args):
-        # print(f'    {args = !r}')
-        # print(f'    {len(args) = }')
-
         arg_tuple = tuple(args)
-        assert len(arg_tuple) == cls.len, f'{cls.__name__} must have length {cls.len}'
+        assert len(arg_tuple) == cls.len, \
+            f'{cls.__name__} must have length {cls.len}'
         typed_args = tuple(cls.scalar_type(a) for a in args)
         r = super().__new__(cls, typed_args)
         return r
@@ -117,34 +129,60 @@ class _Vector(tuple, _WgslType):
         return (self.scalar_type.dtype, (self.len, ))
 
 class vec2f(_Vector):
+    """A two element vector of floats"""
     scalar_type = f32
     len = 2
     align = 2 * f32.align
 
 class vec3f(_Vector):
+    """A three element vector of floats"""
     scalar_type = f32
     len = 3
     align = 4 * f32.align       # !!!
 
 class vec4f(_Vector):
+    """A four element vector of floats"""
     scalar_type = f32
     len = 4
     align = 4 * f32.align
 
+class vec2i(_Vector):
+    """A two element vector of signed ints"""
+    scalar_type = i32
+    len = 2
+    align = 2 * i32.align
+
+class vec3i(_Vector):
+    """A three element vector of signed ints"""
+    scalar_type = i32
+    len = 3
+    align = 4 * i32.align       # !!!
+
+class vec4i(_Vector):
+    """A four element vector of signed ints"""
+    scalar_type = i32
+    len = 4
+    align = 4 * i32.align
+
 class vec2u(_Vector):
+    """A two element vector of unsigned ints"""
     scalar_type = u32
     len = 2
     align = 2 * u32.align
 
 class vec3u(_Vector):
+    """A three element vector of unsigned ints"""
     scalar_type = u32
     len = 3
     align = 4 * u32.align       # !!!
 
 class vec4u(_Vector):
+    """A four element vector of unsigned ints"""
     scalar_type = u32
     len = 4
     align = 4 * u32.align
+
+# Vector Unit Tests
 
 v = vec2f(3, 4)
 assert v == (3, 4)
@@ -179,25 +217,18 @@ assert isinstance(v, vec3u)
 
 del v
 
-# verbose = False
 
+## ##  ##   ##    ##     ##      ##       ##      ##     ##    ##   ##  ## ##
+## Uniforms
 
 class _FieldInfo(namedtuple('_FieldInfo', 'name offset bytes align')):
     pass
 
-
-class UniformMeta(type):
+class _UniformMeta(type):
 
     def __new__(cls, *args, **kwargs):
         """Create a Uniforms class.  Give it dataclass behavior."""
-        # if verbose:
-        #     print(f'UniformMeta.__new__')
-        #     print(f'    {cls = }')
-        #     print(f'    {args = }')
-        #     print(f'    {kwargs = }')
         cls_obj = super().__new__(cls, *args, **kwargs)
-        # if verbose:
-        #     print(f'    {cls_obj = }')
 
         # Specify kw_only, otherwise dataclass forces fields without
         # default values to the front of the struct.
@@ -205,67 +236,111 @@ class UniformMeta(type):
 
     def __init__(self, name, bases, namespace, **kwargs):
 
+        """Create a Uniforms class.  Verify that all annotated fields
+           have a WGSL type.
         """
-            Create a Uniforms class.  Verify that all annotated fields
-            have a WGSL type.
-        """
-
-        # print(f'UniformMeta.__init__')
-        # print(f'    {self = }')
-        # print(f'    {name = }')
-        # print(f'    {len(kwargs) = }')
-
         annotations = get_annotations(self)
-        # if verbose:
-        #     print(f'    {annotations = }')
         for (f, t) in annotations.items():
-            assert issubclass(t, _WgslType), f'uniform field {f!r} must be a WGSL type'
+            assert issubclass(t, _WgslType), \
+                f'uniform field {f!r} must be a WGSL type'
 
         for f in self.__dict__:
             if f.startswith('__'):
                 continue
-            # print(f'    {f = }')
-            # print(f'    {self.__dict__[f] = }')
             if isfunction(self.__dict__[f]):
                 continue
             if isinstance(self.__dict__[f], classmethod):
                 continue
             if isinstance(self.__dict__[f], _classproperty):
                 continue
-            assert f in annotations, f'uniform field {f!r} must have a type annotation'
+            assert f in annotations, \
+                f'uniform field {f!r} must have a type annotation'
 
         r = super().__init__(name, bases, namespace, **kwargs)
-        # if verbose:
-        #     print(f'    {r = }')
         return r
 
-class Uniforms(metaclass=UniformMeta):
+class Uniforms(metaclass=_UniformMeta):
+
+    """An abstract class that allows easy definition of uniform buffers.
+    
+    Example:
+
+        class MyUniforms(Uniforms):
+            scale: vec2f = (1, 1)
+            brightness: f32 = 1
+            index: u32  # no default value
+
+    A Uniforms subclass is a dataclass (as defined in Python's
+    standard library), so it has dataclass's behaviors.  Example:
+
+        # Instantiate into a Python object
+        my_uniforms = MyUniforms(index=3)
+        my_uniforms = MyUniforms(index=4, scale=[2, 3])
+
+        # print(my_uniforms)
+        MyUniforms(scale=(2.0, 3.0), brightness=1.0, index=4)
+
+    A Uniforms subclass enforces things.
+
+      - Fields must be annoated as WGSL types (scalars and vectors)
+      - Fields can only contain the declared types; values are coerced
+        to the right type on initialization and assignment
+    
+    Uniforms subclasses have the same bytes and dtypes properties
+    described above.
+
+    Additional behavior:
+
+      - create_buffer(device, **kwargs) - tell a wgpu device to
+        create a buffer that matches the struct definition
+
+        For example,
+
+            MyUniforms.create_buffer(device)
+
+        is equivalent to
+
+            device.create_buffer(
+                label='my_uniforms buffer',
+                size=16,
+                usage=wgpu.BufferUsage.UNIFORM | wgpu.BufferUsage.COPY_DST,
+            )
+
+        You can override the `label`, `size`, and `usage` parameters
+        with keyword args.
+
+      - as_data() - emit a numpy structured array with the correct binary
+        layout to write to the GPU
+
+    A Uniforms
+
+        # Create WGPU uniform buffer
+        my_u_buffer = MyUniforms.create_buffer(device,
+            visibility=Vertex | Fragment)
+
+        # Update values
+        my_uniforms.scale = (1.4, 1.4)
+
+        # send to GPU
+        my_uniforms.write_buffer(device, my_u_buffer)
+"""
 
     def __setattr__(self, name, value):
-        """ set a field.  Coerce the new value to the field's type. """
+        # set a field.  Coerce the new value to the field's type.
 
         annotations = get_annotations(self.__class__)
-        assert name in annotations, f'unknown field {name!r} in {self.__class__.__name__}'
-#         print(f'__setattr__')
-# #        print(f'    {self = }')
-#         print(f'    {name = }, {value = }')
+        assert name in annotations, \
+            f'unknown field {name!r} in {self.__class__.__name__}'
 
         # ugly hack: if setting a vector, unpack its args
         def cast(name, value):
-            # print(f'    cast({name=}, {value=})')
-            # print(f'        {annotations[name] = }')
-            # print(f'        {hasattr(annotations[name], 'scalar_type') = }')
             anno = annotations[name]            
             if hasattr(anno, 'scalar_type'):
-                # print(f'        has scalar_type')
                 return anno(*value)
             return anno(value)
 
         cast_value = cast(name, value)
-        # if verbose:
-        #     print(f'setattr({name}, {value}) => {cast_value}')
         super().__setattr__(name, cast_value)
-
 
     @_classproperty
     @cache
@@ -274,16 +349,11 @@ class Uniforms(metaclass=UniformMeta):
         offset = 0
         align = None
         for (fname, fclass) in get_annotations(cls).items():
-            # print(f'iteration')
             bytes = fclass.bytes
             align = fclass.align
-            # print(f'    {bytes = }, {align = }')
-            # print(f'    A: {offset = }')
             offset += (align - offset) % align
-            # print(f'     B: {offset = }')
             info += [_FieldInfo(fname, offset, bytes, align)]
             offset += bytes
-            # print(f'     C: {offset = }')
         return tuple(info)
 
     @_classproperty
@@ -298,7 +368,7 @@ class Uniforms(metaclass=UniformMeta):
     @_classproperty
     @cache
     def dtype(cls):
-        # print(f'{cls.__name__}.dtype')
+        """Return a numpy dtype definition"""
         annotations = get_annotations(cls)
         info = cls._field_info
         dt = []
@@ -318,15 +388,22 @@ class Uniforms(metaclass=UniformMeta):
 
     @classmethod
     def create_buffer(cls, device, **kwargs):
+        """Tell wgpu to create a suitable uniform buffer
+
+            Keyword arguments:
+              label -- wgpu buffer label (default: 'class_name buffer')
+              usage -- wgpu bufer usage (default: UNIFORM | COPY_DST)
+        """
 
         kwargs.setdefault('label', f'{camel_to_snake(cls.__name__)} buffer')
         kwargs.setdefault('size', cls.bytes)
         kwargs.setdefault('usage', BufferUsage.UNIFORM | BufferUsage.COPY_DST)
 
-        # print(f'{kwargs = }')
         return device.create_buffer(**kwargs)
 
     def as_data(self):
+        """Return a data object with GPU-compatible binary layout."""
+
         data = np.zeros((), dtype=self.dtype)
         for info in self._field_info:
             data[info.name] = getattr(self, info.name)
@@ -342,6 +419,8 @@ class Uniforms(metaclass=UniformMeta):
 
         # What's the memory?  
 
+# Uniforms unit tests
+
 class Test(Uniforms):
     index: u32 = 42
     ix2: u32 = 42 / 4
@@ -349,18 +428,15 @@ class Test(Uniforms):
     pt: vec4f = (1, 2, 3, 4.4)
 
 test = Test()
-# print(test)
 assert test.index == 42
 assert test.pt == (1, 2, 3, 4.4)
 assert type(test.index) is u32
 assert type(test.pt) is vec4f
-# print('\n'.join(str(o) for o in test._field_info))
 assert test._field_info[0].offset == 0
 assert Test._field_info[1].offset == 4
 assert test._field_info[2].offset == 16
 assert Test._field_info[3].offset == 32
 assert test.bytes == 48
-# print(test.dtype)
 assert test.dtype == [
     ('index',    'u4'),
     ('ix2',      'u4'),
@@ -371,14 +447,12 @@ assert test.dtype == [
     ]
 
 test = Test(pt=(5.5, 6.6, 7.7, 8.8))
-# print(test)
 assert test.index == 42
 assert test.pt == (5.5, 6.6, 7.7, 8.8)
 assert type(test.index) is u32
 assert type(test.pt) is vec4f
 
 test.index = 5.5
-# print(test)
 assert test.index == 5
 assert test.pt == (5.5, 6.6, 7.7, 8.8)
 assert type(test.index) is u32
@@ -387,4 +461,4 @@ assert type(test.pt) is vec4f
 data = test.as_data()
 assert np.all(data['p3'] == test.p3)
 
-del test
+del Test, test
